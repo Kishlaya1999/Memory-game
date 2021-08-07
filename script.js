4fda18684ba1b4f0635153c6006c8777d6ae7d35
Kishlaya1999 <kishlaya54321@gmail.com>
2021-08-07 17:08:45 +0530

    Level menu added

--- script.js
+++ script.js
@@ -19,9 +19,29 @@ const pictures = [
      "./Images/Youtube.png"
 ]
 
-let gameContainer = document.getElementById("gameContainer");
+const gameContainer = document.getElementById("gameContainer");
+const selectedLevel = document.getElementById("selectedLevel");
+const setLevel = document.getElementById("setLevel");
 
 document.addEventListener("DOMContentLoaded", function () {
+
+     setLevel.addEventListener("click", function (e) {
+     e.preventDefault();
+     gameContainer.innerHTML = " "; // if we will not include it then cardContainerInsert will be added in the existing set of cards thats why we enpty it before inserting new card container
+     let level = selectedLevel.value;
+     let row = level.slice(0, 1);
+     let column = level.slice(2, 3);
+     for (let j = 1; j <= row * column; j++) {
+          gameContainer.innerHTML +=
+               `<div class="cardContainer">
+                    <div class="front"><img src="./Images/brain.png" alt=""></div>
+                    <div class="back"><img src="${pictures[j]}" alt=""></div>
+               </div>`;
+     }
+     gameContainer.style.gridTemplateColumns = `repeat( ${column} ,1fr)`;
+     gameContainer.style.gridTemplateRows = `repeat( ${row} ,1fr)`;
+});
+
      for (let i = 0; i < 16; i++) {
           gameContainer.innerHTML +=
                `<div class="cardContainer">
