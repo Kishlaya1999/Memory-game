4cc19b8a3c012da60a838b948f783c8519d155bf
Kishlaya1999 <kishlaya54321@gmail.com>
2021-08-07 23:00:08 +0530

    Comments added in JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+//TODO:Shuffling pictures array function
+//TODO:Think about the logic for inserting pictures randomly two times
+//TODO:Create moves and scores section
+//TODO:Close privious two cards if third one is opened and increment the moves
 const pictures = [
      "./Images/AngularJS.png",
      "./Images/bootstrap.png",
@@ -23,41 +27,41 @@ const gameContainer = document.getElementById("gameContainer");
 const selectedLevel = document.getElementById("selectedLevel");
 const setLevel = document.getElementById("setLevel");
 
-document.addEventListener("DOMContentLoaded", function () {
-     setLevel.addEventListener("click", function (e) {
-          e.preventDefault();
-          gameContainer.innerHTML = " "; // if we will not include it then cardContainerInsert will be added in the existing set of cards thats why we enpty it before inserting new card container
-          let level = selectedLevel.value;
-          let row = level.slice(0, 1);
-          let column = level.slice(2, 3);
-          for (let j = 1; j <= row * column; j++) {
-               gameContainer.innerHTML +=
-                    `<div class="cardContainer">
-                    <div class="front"><img src="./Images/brain.png" alt=""></div>
-                    <div class="back"><img src="${pictures[j]}" alt=""></div>
-                    </div>`;
-          }
-          gameContainer.style.gridTemplateColumns = `repeat( ${column} ,1fr)`;
-          gameContainer.style.gridTemplateRows = `repeat( ${row} ,1fr)`;
-          cardClick();
-     });
-
-     for (let i = 0; i < 16; i++) {
+document.addEventListener("DOMContentLoaded", function () {      //inserting the cards in page on loadig 
+     for (let i = 0; i < 16; i++) {          //by default level is set to 4x4 so 16 cards are inserted in the page
           gameContainer.innerHTML +=
                `<div class="cardContainer">
                     <div class="front"><img src="./Images/brain.png" alt=""></div>
                     <div class="back"><img src="${pictures[i]}" alt=""></div>
                </div>`
      }
-
      cardClick();
 })
 
-function cardClick() {
-     document.querySelectorAll(".cardContainer").forEach(function (card) {
-          card.addEventListener("click", function (e) {
+setLevel.addEventListener("click", function(e) {
+     e.preventDefault();
+     gameContainer.innerHTML = " "; // if we will not include it then cardContainerInsert will be added in the existing set of cards thats why we enpty it before inserting new card container
+     let level = selectedLevel.value; //taking the value from dropdown list
+     let row = level.slice(0, 1);  //slicing the number of rows from dropdown list
+     let column = level.slice(2, 3);    //slicing the number of columns from dropdown list
+     for (let j = 1; j <= row * column; j++) {    //inserting the number of cards according to the level selected
+          gameContainer.innerHTML +=
+               `<div class="cardContainer">
+                    <div class="front"><img src="./Images/brain.png" alt=""></div>
+                    <div class="back"><img src="${pictures[j]}" alt=""></div>
+               </div>`;
+     }
+     gameContainer.style.gridTemplateColumns = `repeat( ${column} ,1fr)`;  //changing the number of columns depending on the level selected in css grid
+     gameContainer.style.gridTemplateRows = `repeat( ${row} ,1fr)`;   //changing the number of rows depending on the level selected in css grid
+     cardClick();
+});
+
+//A function that adds event listner in all the element with cardContainer class
+let cardClick = () => {       
+     document.querySelectorAll(".cardContainer").forEach(function (card) {      //selecting all the elements with class 'cardContainer'
+          card.addEventListener("click", function (e) {     //adding event listener in all the cards
                e.preventDefault();
-               console.log(this.childNodes);
+               // console.log(this.style);
                let rotateCard = e.currentTarget;
                rotateCard.style.transform = "rotateY(180deg)";
                setTimeout(function () {
