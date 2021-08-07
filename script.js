8073128a0903053f07a26fb4a92e517f640fc074
Kishlaya1999 <kishlaya54321@gmail.com>
2021-08-07 20:07:17 +0530

    Bug fixed, now rotation of cards can be done in all levels

--- script.js
+++ script.js
@@ -24,23 +24,23 @@ const selectedLevel = document.getElementById("selectedLevel");
 const setLevel = document.getElementById("setLevel");
 
 document.addEventListener("DOMContentLoaded", function () {
-
      setLevel.addEventListener("click", function (e) {
-     e.preventDefault();
-     gameContainer.innerHTML = " "; // if we will not include it then cardContainerInsert will be added in the existing set of cards thats why we enpty it before inserting new card container
-     let level = selectedLevel.value;
-     let row = level.slice(0, 1);
-     let column = level.slice(2, 3);
-     for (let j = 1; j <= row * column; j++) {
-          gameContainer.innerHTML +=
-               `<div class="cardContainer">
+          e.preventDefault();
+          gameContainer.innerHTML = " "; // if we will not include it then cardContainerInsert will be added in the existing set of cards thats why we enpty it before inserting new card container
+          let level = selectedLevel.value;
+          let row = level.slice(0, 1);
+          let column = level.slice(2, 3);
+          for (let j = 1; j <= row * column; j++) {
+               gameContainer.innerHTML +=
+                    `<div class="cardContainer">
                     <div class="front"><img src="./Images/brain.png" alt=""></div>
                     <div class="back"><img src="${pictures[j]}" alt=""></div>
-               </div>`;
-     }
-     gameContainer.style.gridTemplateColumns = `repeat( ${column} ,1fr)`;
-     gameContainer.style.gridTemplateRows = `repeat( ${row} ,1fr)`;
-});
+                    </div>`;
+          }
+          gameContainer.style.gridTemplateColumns = `repeat( ${column} ,1fr)`;
+          gameContainer.style.gridTemplateRows = `repeat( ${row} ,1fr)`;
+          cardClick();
+     });
 
      for (let i = 0; i < 16; i++) {
           gameContainer.innerHTML +=
@@ -50,8 +50,13 @@ document.addEventListener("DOMContentLoaded", function () {
                </div>`
      }
 
+     cardClick();
+})
+
+function cardClick() {
      document.querySelectorAll(".cardContainer").forEach(function (card) {
           card.addEventListener("click", function (e) {
+               e.preventDefault();
                console.log(this.childNodes);
                let rotateCard = e.currentTarget;
                rotateCard.style.transform = "rotateY(180deg)";
@@ -59,6 +64,6 @@ document.addEventListener("DOMContentLoaded", function () {
                     // console.log(rotateCard.style.transform)
                     rotateCard.style.transform = "rotateY(0deg)";
                }, 3000);
-          });
-     });
-})
+          })
+     })
+}
