18c9f10a7b17426b165ea6551155fa337b813a79
Kishlaya1999 <kishlaya54321@gmail.com>
2021-08-07 13:05:26 +0530

    Rotation functionality added

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 const pictures = [
      "./Images/AngularJS.png",
      "./Images/bootstrap.png",
-     "./Images/chrome.png",
      "./Images/css3.png",
-     "./Images/css-tricks.png",
-     "./Images/emmet.png",
-     "./Images/git.png",
-     "./Images/github.png",
      "./Images/html.png",
      "./Images/javaScript.png",
-     "./Images/Live Server.png",
      "./Images/reactJS.png",
-     "./Images/scss.png",
-     "./Images/stackoverflow.png",
      "./Images/tailwind.png",
-     "./Images/vscode.png",
      "./Images/vuejs.png",
-     "./Images/Youtube.png",
+     "./Images/chrome.png",
+     "./Images/css-tricks.png",
+     "./Images/github.png",
+     "./Images/emmet.png",
+     "./Images/git.png",
+     "./Images/scss.png",
+     "./Images/Live Server.png",
+     "./Images/stackoverflow.png",
+     "./Images/vscode.png",
+     "./Images/Youtube.png"
 ]
 
 let gameContainer = document.getElementById("gameContainer");
@@ -29,4 +29,16 @@ document.addEventListener("DOMContentLoaded", function () {
                     <div class="back"><img src="${pictures[i]}" alt=""></div>
                </div>`
      }
+
+     document.querySelectorAll(".cardContainer").forEach(function (card) {
+          card.addEventListener("click", function (e) {
+               console.log(this.childNodes);
+               let rotateCard = e.currentTarget;
+               rotateCard.style.transform = "rotateY(180deg)";
+               setTimeout(function () {
+                    // console.log(rotateCard.style.transform)
+                    rotateCard.style.transform = "rotateY(0deg)";
+               }, 3000);
+          });
+     });
 })
